e70d8be2d720b22fa73ecc1947e81fa38c24d7e8
Anjali Bhokare <anjalibhokare1306@gmail.com>
2026-08-01 20:44:50 +0530

    Time: 66 ms (23.33%), Space: 42.5 MB (94.79%) - LeetHub

---
+++ 0486-predict-the-winner/0486-predict-the-winner.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean predictTheWinner(int[] nums) {
+        return solve(nums, 0, nums.length - 1) >= 0;
+    }
+    public int solve(int[] nums, int left, int right) {
+        if (left == right)
+            return nums[left];
+        int pickL= nums[left] - solve(nums, left + 1, right);
+        int pickR= nums[right] - solve(nums, left, right - 1);
+        return Math.max(pickL, pickR);
+    }
+}
