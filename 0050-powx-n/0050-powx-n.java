78097da04ea500cd647de4aa9c6da69beee43632
Anjali Bhokare <anjalibhokare1306@gmail.com>
2026-08-03 19:46:56 +0530

    Time: 0 ms (100%), Space: 48.5 MB (11.52%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,18 +1,18 @@
 class Solution {
     public double myPow(double x, int n) {
-        return pow(x, (long) n);
+        return pow(x,n);
     }
-    private double pow(double x, long n) {
-        if (n == 0) {
+    public double pow(double x, long n) {
+        if (n==0) {
             return 1;
         }
-        if (n < 0) {
-            return 1.0 / pow(x, -n);
+        if (n<0) {
+            return 1.0 / pow(x,-n);
         }
-        if (n % 2 == 1) {
-            return x * pow(x * x, (n - 1) / 2);
+        if (n%2==1) {
+            return x*pow(x*x,(n-1)/2);
         } else {
-            return pow(x * x, n / 2);
+            return pow(x*x,n/2);
         }
     }
 }
