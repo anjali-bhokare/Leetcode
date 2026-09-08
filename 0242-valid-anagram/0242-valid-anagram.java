d503cf2e210546ff49d854508a990ea39fbb7650
Anjali Bhokare <anjalibhokare1306@gmail.com>
2026-09-08 21:33:45 +0530

    Time: 5 ms (63.04%), Space: 43.9 MB (97.66%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,9 +1,9 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        if (s.length() != t.length())
+        if (s.length()!= t.length())
             return false;
         int[] arr = new int[26];
-        for (int i = 0; i < s.length(); i++) {
+        for (int i =0;i<s.length();i++) {
             arr[s.charAt(i) - 'a']++;
             arr[t.charAt(i) - 'a']--;
         }
@@ -11,7 +11,6 @@ class Solution {
             if (arr[i]!= 0)
                 return false;
         }
-
         return true;
     }
 }
